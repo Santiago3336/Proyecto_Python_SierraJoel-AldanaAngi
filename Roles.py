9368505fce1e7718d14614e7dc8de05f3d923e7a
Santiago <j.santiagolseon@gmail.com>
2024-02-11 07:20:55 -0500

    menu principal Rol coordinador

--- Roles.py
+++ Roles.py
@@ -1,3 +1,17 @@
-print('¿Quien eres?')
+# print('¿Quien eres?')
 
-roles = input('')
+# roles = input('')
+
+def menuCoord():
+    print('''Bienvenido coordinacion
+          \nA continuacion ingresa al numero de la opcion que deseas ejecutar: 
+          \n1 Campers
+          \n2 Trainers
+          \n3 Horarios
+          \n4 Modulos
+          \n5 Salones
+          \n6 Rutas
+          \n7 Reportes''')
+
+def coordinador():
+    menuCoord()
