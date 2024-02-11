648b4848a548ef9bcc6b5c95fbe810733fa88519
Santiago <j.santiagolseon@gmail.com>
2024-02-10 23:19:57 -0500

    voceto de sub menus

--- modulo.py
+++ modulo.py
@@ -10,4 +10,13 @@ def coordinador():
     print("A cual menu deseas entrar?\n1 Campers\n2 Trainers\n3 Horarios\n4 Modulos\n5 Salones\n6 Rutas")
     action = input()
     if action == 1:
-        print("A continuacion escribe el numero de la opcion a la que deseas ingresar:\n2 Campers en Riesgo Alto\n")
+        print("A continuacion escribe el numero de la opcion a la que deseas ingresar:\n1 Campers en Riesgo Alto\n")
+    elif action == 2:
+        print()
+    elif action == 3:
+        print()
+    elif action == 4:
+        print()
+    elif action == 6:
+        print()
+        print("Que deseas hacer?\n1 Añadir nuevas Rutas\t2 Eliminar una Ruta\t3 Reemplazar una Ruta")
