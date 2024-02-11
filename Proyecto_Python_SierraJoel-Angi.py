b80dcc3c1bd042fd79c9e8dcd911f51249f1b08c
Santiago <j.santiagolseon@gmail.com>
2024-02-10 22:55:59 -0500

    Inicializacion

---
+++ Proyecto_Python_SierraJoel-Angi.py
@@ -0,0 +1,8 @@
+import json
+
+
+
+who = input("Hola, Quien eres?")
+
+if who == '':
+    who = coordinador
---
+++ modulo.py
@@ -0,0 +1,13 @@
+import json
+
+action = str
+
+def nextId():
+    id + 1
+
+
+def coordinador():
+    print("A cual menu deseas entrar?\n1 Campers\n2 Trainers\n3 Horarios\n4 Modulos\n5 Salones\n6 Rutas")
+    action = input()
+    if action == 1:
+        print("A continuacion escribe el numero de la opcion a la que deseas ingresar:\n2 Campers en Riesgo Alto\n")
